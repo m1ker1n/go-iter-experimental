800d61f157d1cbb2855a5de9ac917086620c1e84
m1ker1n <m1ker1nigor@yandex.ru>
2024-02-17 20:01:04 +0300

    fixes

--- main.go
+++ main.go
@@ -6,34 +6,6 @@ import (
 	"math/rand"
 )
 
-//func ReadTextFileByLines(r io.Reader) iter.Seq2[string, error] {
-//	scanner := bufio.NewScanner(r)
-//	return func(yield func(string, error) bool) {
-//		for scanner.Scan() {
-//			if !yield(scanner.Text(), nil) {
-//				return
-//			}
-//		}
-//		if err := scanner.Err(); err != nil {
-//			yield("", err)
-//		}
-//	}
-//}
-//
-//func main1() {
-//	file, err := os.Open("README.md")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	for text, err := range ReadTextFileByLines(file) {
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		fmt.Println(text)
-//	}
-//}
-
 // RandomOrderIterator iterates through s in random order returning index and value of each element.
 func RandomOrderIterator[S ~[]E, E any](s S) iter.Seq2[int, E] {
 	order := rand.Perm(len(s))
@@ -47,8 +19,31 @@ func RandomOrderIterator[S ~[]E, E any](s S) iter.Seq2[int, E] {
 }
 
 func main() {
-	s := []int{5, 10, 4, 7, 3}
-	for i, v := range RandomOrderIterator(s) {
-		fmt.Printf("s[%d]=%d\n", i, v)
+	s1 := []int{5, 10, 4, 7, 3}
+	for i, v := range RandomOrderIterator(s1) {
+		fmt.Printf("s1[%d]=%v\n", i, v)
+	}
+	fmt.Println()
+
+	s2 := []string{"five", "ten", "four", "seven", "three"}
+	for i, v := range RandomOrderIterator(s2) {
+		fmt.Printf("s2[%d]=%v\n", i, v)
+	}
+	fmt.Println()
+
+	type someStruct struct {
+		i   int
+		str string
+	}
+
+	s3 := []someStruct{
+		{5, "five"},
+		{10, "ten"},
+		{4, "four"},
+		{7, "seven"},
+		{3, "three"},
+	}
+	for i, v := range RandomOrderIterator(s3) {
+		fmt.Printf("s3[%d]=%v\n", i, v)
 	}
 }
